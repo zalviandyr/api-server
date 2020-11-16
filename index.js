0ce056f54cab6ebbff240d5477439bec0887bcde
Zukron Alviandy <zukronalviandy@gmail.com>
2020-11-16 10:06:57 +0700

    update yt-video endpoint

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 const fs = require('fs')
 const express = require('express')
+const ytdl = require('ytdl-core')
 
 // my library
 const endpoint = require('./lib/endpoint')
+const { formatBytes, getVideoID } = require('./lib/helpers')
 
 const app = express()
+const api = express.Router()
 const port = process.env.PORT || 4000
 
-app.get('/', async (req, res) => {
-    await endpoint.ytVideo(req.query)
+api.get('/yt-video', (req, res) => {
+    endpoint.ytVideo(req.headers.host, req.query)
         .then((result) => {
             res.send(result)
-        })
-        .catch(() => {
-            res.send('gagal')
+        }).catch((err) => {
+            res.send(err.message)
         })
 })
 
-app.get('/temp', (req, res) => {
-    res.send('hello world')
+api.get('/temp', (req, res) => {
+    // const url = 'https://www.youtube.com/watch?v=R19uQyfwqhg'
+    // const url = 'https://www.youtube.com/watch?v=kUX9sPALG5g'
+    try {
+        const url = 'https://www.youtube.com/watch?v=kPA@SDg'
+        console.log(getVideoID(url))
+    } catch (err) {
+        console.log(err.message)
+    }
+    res.send(formatBytes(50000000).toString())
 })
 
+api.get('/yt-video/download', (req, res) => {
+    const videoID = req.query.id
+    const title = req.query.title
+    const ext = req.query.ext
+    const fileName = title + '.' + ext
+    res.header('Content-Disposition', `attachment; filename=${fileName}`)
+
+    const url = `https://www.youtube.com/watch?v=${videoID}`
+    ytdl(url, { filter: (_format) => _format.container === 'mp4' })
+        .pipe(res)
+})
+
+// start server
+app.use('/api', api)
 app.listen(port, () => {
+    // TODO delete this line
     fs.mkdirSync('./storage', { recursive: true })
     console.log(`server started at port: ${port}`)
 })
