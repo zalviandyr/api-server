b5b5582d5eff544356e9ef31b455b6a1d7875f73
Zukron Alviandy <zukronalviandy@gmail.com>
2020-11-25 10:19:15 +0700

    add quran endpoint

--- index.js
+++ index.js
@@ -214,6 +214,13 @@ api.post('/what-anime', (req, res) => {
         }).catch((err) => res.status(err.status_code).json(err))
 })
 
+api.get('/quran', (req, res) => {
+    endpoint.quran()
+        .then((result) => {
+            res.send(result)
+        }).catch((err) => res.status(err.status_code).json(err))
+})
+
 // base url
 api.get('/', (req, res) => {
     res.send('API Server by Zukron Alviandy R')
