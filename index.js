7d55ef1b48f1e07ae491d01958b25095528653bc
Zukron Alviandy <zukronalviandy@gmail.com>
2020-12-02 11:33:39 +0700

    add movie endpoint

--- index.js
+++ index.js
@@ -213,6 +213,13 @@ api.get('/lirik', (req, res) => {
         }).catch((err) => res.status(err.status_code).json(err))
 })
 
+api.get('/movie', (req, res) => {
+    endpoint.movie(req.query)
+        .then((result) => {
+            res.send(result)
+        }).catch((err) => res.status(err.status_code).json(err))
+})
+
 // base url
 api.get('/', (req, res) => {
     res.send('API Server by Zukron Alviandy R')
