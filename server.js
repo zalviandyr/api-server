1bab4bcb13c798e19352eeae0061b32164b4d76a
Zukron Alviandy <zukronalviandy@gmail.com>
2020-12-07 11:13:01 +0700

    fix bug

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 require('module-alias/register')
-const express = require('express')
 
 const { Route } = require('cores/Route')
-const { Module } = require('configs/Module')
+const { app } = require('./app')
 
 class App extends Route {
     init() {
         const port = 4000
-        const app = express()
-
-        const mod = new Module(app)
-        mod.bodyParser()
-        mod.form()
 
         app.enable('trust proxy')
         app.use('/api', super.init())
