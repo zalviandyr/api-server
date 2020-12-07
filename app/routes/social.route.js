45930bf71679788ed1524646148687b745fe320a
Zukron Alviandy <zukronalviandy@gmail.com>
2020-12-07 14:17:52 +0700

    add ig endpoint

--- app/routes/social.route.js
+++ app/routes/social.route.js
@@ -1,5 +1,6 @@
 const { FbVideoController } = require('controllers/social/fb-video.controller')
 const { IgProfileController } = require('controllers/social/ig-profile.controller')
+const { IgController } = require('controllers/social/ig.controller')
 const { Controller } = require('cores/Controller')
 
 class SocialRoute extends Controller {
@@ -7,6 +8,7 @@ class SocialRoute extends Controller {
         return [
             this.get('/fb-video', (req, res) => new FbVideoController(req, res).controller()),
             this.get('/ig-profile', (req, res) => new IgProfileController(req, res).controller()),
+            this.get('/ig', (req, res) => new IgController(req, res).controller()),
         ]
     }
 }
