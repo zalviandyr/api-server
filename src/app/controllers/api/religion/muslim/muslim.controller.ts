27a7238fd91fa8416719e1c6cf24a8c856b4d7b4
zalviandyr <zukronalviandy@gmail.com>
2021-08-18 20:05:20 +0700

    refactor jadwal shalat

--- src/app/controllers/api/religion/muslim/muslim.controller.ts
+++ src/app/controllers/api/religion/muslim/muslim.controller.ts
@@ -20,8 +20,7 @@ export default class MuslimController extends Controller {
     const url = `https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/${kota}/${today[0]}/${today[1]}.json`;
 
     const { data } = await axios.get(url);
-    const dateToday = parseInt(today[2]) - 1;
-    return this.successResponse(data[dateToday]);
+    return this.successResponse(data);
   }
 
   async bacaanShalat(): Promise<Response> {
